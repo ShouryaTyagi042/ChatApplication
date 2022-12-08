e70b7e40ea9925def6d20c5f8835ca7814b26556
Shourya Tyagi <shouryatyagi042@gmail.com>
2022-12-08 18:41:42 +0530

    Defined get and post message functions

--- server.js
+++ server.js
@@ -8,3 +8,30 @@ var server = app.listen(3000, () => {
 app.use(
   express.static("/Users/shouryatyagi/Desktop/Projects/ChatApplication/public")
 );
+
+var dbUrl = "mongodb://localhost:27017/test";
+mongoose.connect(dbUrl, (err) => {
+  console.log("mongodb connected", err);
+});
+var Message = mongoose.model("Message", { name: String, message: String });
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/messages", (req, res) => {
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  });
+});
+
+app.post("/messages", (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) => {
+    if (err) sendStatus(500);
+    res.sendStatus(200);
+  });
+});
+
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
